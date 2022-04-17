6e6143bd497bf8e35bce41b7857af48ce1dbc151
Ejdeguzman72 <ejdeguzman72@gmail.com>
2022-04-17 16:50:22 -0400

    converting to modal

--- components/contact-info/buttons/AddContactButton.js
+++ components/contact-info/buttons/AddContactButton.js
@@ -2,8 +2,30 @@ import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const AddContactButton = ({ navigation }) => {
+    const [modalVisible, setModalVisible] = useState(false);
     return (
         <View style={styles.view}>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Add Contact Information</Text>
+                        <Pressable
+                            style={[styles.button, styles.buttonClose]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={styles.textStyle}>Close</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
             <TouchableOpacity
                 style={styles.button}
                 onPress={() => navigation.navigate('American')}
